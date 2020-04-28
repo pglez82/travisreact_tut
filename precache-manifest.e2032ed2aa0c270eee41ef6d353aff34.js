edb81298ecba838bf84b46fe7dbcb6e75b010e48
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-28 08:59:37 +0000

    Deploy pglez82/travisreact_tut to github.com/pglez82/travisreact_tut.git:gh-pages

---
+++ precache-manifest.e2032ed2aa0c270eee41ef6d353aff34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8a0a546c2db64720d3c4224bc6e2ce9",
+    "url": "/travisreact_tut/index.html"
+  },
+  {
+    "revision": "1903a6c62903334d9a2d",
+    "url": "/travisreact_tut/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "447086afe8bfee51909b",
+    "url": "/travisreact_tut/static/js/2.e033f11c.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/travisreact_tut/static/js/2.e033f11c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1903a6c62903334d9a2d",
+    "url": "/travisreact_tut/static/js/main.a279cf26.chunk.js"
+  },
+  {
+    "revision": "75153e30a232cd5554e5",
+    "url": "/travisreact_tut/static/js/runtime-main.ef777545.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travisreact_tut/static/media/logo.5d5d9eef.svg"
+  }
+]);
